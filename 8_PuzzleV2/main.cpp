3ac418e075a3e39d97c1eb791dbee98383bcfcd7
davidzhu94 <davidzhu94@gmail.com>
2017-02-20 17:48:38 -0800

    Initial Commit

---
+++ 8_PuzzleV2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  8_PuzzleV2
+//
+//  Created by David Zhu on 2/20/17.
+//  Copyright © 2017 David Zhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
